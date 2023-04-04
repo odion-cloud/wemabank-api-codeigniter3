7b19083d912f2d814badb1689e549f15bf23ded3
MAC <mac@MACs-MacBook-Pro.local>
2023-04-04 10:36:39 +0100

    update

--- application/libraries/Wemabank.php
+++ application/libraries/Wemabank.php
@@ -5,7 +5,7 @@ class Wemabank{
 
     public function lookup_account($account_number)
     {
-       
+       $businessId = "3a000d3b-9760-4d1a-f501-08daf9260b71";
 
         $url = "https://alatpay.azure-api.net/merchant-onboarding/api/v1/banks/accountName";
         
@@ -37,12 +37,31 @@ class Wemabank{
     }
 
 
-    public function virtual_account_create($apikey,$businessId, $amount, $orderid, $email, $phone, $firstname)
+    public function virtual_account_create($apikey, $businessId, $amount, $orderid, $email, $phone, $first_name)
     {
        
+         // $businessId="3a000d3b-9760-4d1a-f501-08daf9260b71";
 
+         // echo $businessId;
 
         $curl = curl_init();
+
+        $fields = array(
+     
+            'transactionId'=> 'f603d89e-35a4-4287-48f8-08db339ed38d',
+            'amount'=> $amount,
+            'currency'=> 'NGN',
+            'orderId'=> '',
+            'businessId'=> $businessId,
+            'businessName'=> null,
+            'description'=> null,
+            'customer'=> array(
+                'email'=>  $email,
+                'phone'=> $phone,
+                'firstName'=> $first_name,
+                'lastName'=> $first_name,
+                'metadata'=> 'null')
+    );
 
         curl_setopt_array($curl, array(
           CURLOPT_URL => 'https://lagos-alat-blueapi.azure-api.net/bank-transfer/api/v1/bankTransfer/virtualAccount',
@@ -53,24 +72,9 @@ class Wemabank{
           CURLOPT_FOLLOWLOCATION => true,
           CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
           CURLOPT_CUSTOMREQUEST => 'POST',
-          CURLOPT_POSTFIELDS =>'{
-            "transactionId": "",
-            "amount": "7500000",
-            "currency": "NGN",
-            "orderId": "",
-            "businessId": "3a000d3b-9760-4d1a-f501-08daf9260b71",
-            "businessName": null,
-            "description": null,
-            "customer": {
-                "email": "danielibanga@gmail.com",
-                "phone": "2347037805351",
-                "firstName": "Daniel",
-                "lastName": "Ibanga",
-                "metadata": "null"
-            }
-        }',
+          CURLOPT_POSTFIELDS => json_encode($fields),
           CURLOPT_HTTPHEADER => array(
-            'Ocp-Apim-Subscription-Key: 1d133fe1bd144b8582ae7c387c00818c',
+            'Ocp-Apim-Subscription-Key: 88a33283cbd74fe8880c8d147605809c',
             'Content-Type: application/json'
           ),
         ));
@@ -102,12 +106,12 @@ class Wemabank{
           CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
           CURLOPT_CUSTOMREQUEST => 'POST',
           CURLOPT_POSTFIELDS =>'{
-    "businessId": "3a000d3b-9760-4d1a-f501-08daf9260b71",
-    "cardNumber": "5123450000000008",
-    "currency": "NGN"
-}',
+                "businessId": "'.$businessId.'",
+                "cardNumber": "5123450000000008",
+                "currency": "NGN"
+            }',
           CURLOPT_HTTPHEADER => array(
-            'Ocp-Apim-Subscription-Key: 1d133fe1bd144b8582ae7c387c00818c',
+            'Ocp-Apim-Subscription-Key: 88a33283cbd74fe8880c8d147605809c',
             'Content-Type: application/json'
           ),
         ));
@@ -134,7 +138,7 @@ class Wemabank{
 
         # Request headers
         $headers = array(
-            'Ocp-Apim-Subscription-Key: 1d133fe1bd144b8582ae7c387c00818c',
+            'Ocp-Apim-Subscription-Key: 88a33283cbd74fe8880c8d147605809c',
             'Content-Type: application/json',
             'Cache-Control: no-cache',);
         curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
@@ -142,12 +146,22 @@ class Wemabank{
         # Request body
         $request_body = '{
           "businessId": "3a000d3b-9760-4d1a-f501-08daf9260b71",
-          "cardNumber": "4173960034038339",
-          "expiryDate": "02/23",
-          "amount": 2000,
-
-          "cvV2": "string",
-          "currency": "NGN"
+          "cardNumber": "5369020005431749",
+          "cardMonth": "02",
+          "cardYear": "23",
+          "securityCode": "702",
+          "amount": 20,
+ 
+          "currency": "NGN",
+          "customer":
+            {
+              "email": "jane.joe@email.com",
+              "phone": "+23480000000001",
+              "firstName": "Jane",
+              "lastName": "+23480000000001",
+              "metadata": "string"
+            },
+           "transactionId": ""
         }';
         curl_setopt($curl, CURLOPT_POSTFIELDS, $request_body);
 
@@ -159,12 +173,33 @@ class Wemabank{
 
     }
 
-    public function authenticate_card($businessId, $amount, $orderid, $email, $phone, $firstname)
+    public function authenticate_card($businessId, $card_number, $cvv, $expiry_date, $amount, $orderid, $email, $phone, $first_name)
     {
        
 
         $url = "https://lagos-alat-blueapi.azure-api.net/paymentcard/api/v1/paymentCard/mc/authenticate";
-        
+        $fields = array(
+     
+            'transactionId'=> '',
+            'cardNumber'=> $card_number,
+            'cardMonth'=> '02',
+            'cardYear'=> '26',
+            'securityCode'=> $cvv,
+            'amount'=> $amount,
+            'currency'=> 'NGN',
+            'orderId'=> '',
+            'businessId'=> $businessId,
+            'businessName'=> '',
+            'description'=> '',
+            'channel' => 'string',
+            'customer'=> array(
+                'email'=>  $email,
+                'phone'=> $phone,
+                'firstName'=> $first_name,
+                'lastName'=> $first_name,
+                'metadata'=> 'null')
+        );
+           
         $curl = curl_init($url);
 
         curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
@@ -173,35 +208,11 @@ class Wemabank{
 
         # Request headers
         $headers = array(
+            'Ocp-Apim-Subscription-Key: 88a33283cbd74fe8880c8d147605809c',
             'Content-Type: application/json',
             'Cache-Control: no-cache',);
         curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-
-        # Request body
-        $request_body = '{
-              "cardNumber": "4173960034038339",
-              "cardMonth": "3",
-              "cardYear": "25",
-              "securityCode": "100",
-              "businessId": "3a000d3b-9760-4d1a-f501-08daf9260b71",
-              "businessName": "",
-              "amount": 1000,
-              "currency": "NGN",
-              "orderId": "",
-              "description": "ALATpay Checkout Payment",
-              "channel": "string",
-              "customer": {
-                "email": "jane.joe@email.com",
-                "phone": "+2348000000001",
-                "firstName": "Jane",
-                "lastName": "Joe",
-                "metadata": ""
-              },
-              "transactionId": "string"
-            }
-               
-        }';
-        curl_setopt($curl, CURLOPT_POSTFIELDS, $request_body);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($fields));
 
         $resp = curl_exec($curl);
         curl_close($curl);
